f0507e4687a2268ac04a46bb24b1a2402d85ef0a
Atakan <atakanhim@gmail.com>
2023-03-01 22:54:54 +0300

    commit eklendi

--- github-proje/app.js
+++ github-proje/app.js
@@ -1,62 +1,58 @@
 const githubForm = document.getElementById("github-form");
-const nameInput =document.getElementById("githubname");
-const clearLastUsers =document.getElementById("clear-last-users");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
 
-const lastUsers=document.getElementById("last-users");
+const lastUsers = document.getElementById("last-users");
 
-const git=new Github();
+const git = new Github();
 const ui = new UI();
-const storage= new Storage
+const storage = new Storage(); // olmasada olurmuş
 eventListeners();
 
-function eventListeners(){
-    githubForm.addEventListener("submit",getData);
-    clearLastUsers.addEventListener("click",clearAllSearched);
-    document.addEventListener("DOMContentLoaded",getAllSearched);
+function eventListeners() {
+  githubForm.addEventListener("submit", getData);
+  clearLastUsers.addEventListener("click", clearAllSearched);
+  document.addEventListener("DOMContentLoaded", getAllSearched);
 }
-function getData(e){
+function getData(e) {
+  let username = nameInput.value.trim(); //boşlluk siler trim
+  if (username === "") {
+    alert("geçerli gir");
+  } else {
+    git
+      .getGithubData(username)
+      .then((response) => {
+        if (response.user.message === "Not Found") {
+          ui.showError("Kullanıcı Bulunamadı");
+        } else {
+          ui.addSearchedUserToUI(username);
+          Storage.addSearchedUserToStorage(username);
+          ui.showUserInfo(response.user);
+          ui.showRepoInfo(response.repo);
+        }
+      })
+      .catch((err) => ui.showError(err));
+  }
 
-    
-    let username = nameInput.value.trim();//boşlluk siler trim
-    if(username ===""){
-        alert("geçerli gir");
-    }
-    else{
-        git.getGithubData(username)
-        .then(response => {
-            if(response.user.message ==="Not Found"){
-                ui.showError("Kullanıcı Bulunamadı");
-            }
-            else{
-                ui.addSearchedUserToUI(username);
-                Storage.addSearchedUserToStorage(username);
-                ui.showUserInfo(response.user);
-                ui.showRepoInfo(response.repo);
-                
-            }
-        })
-        .catch(err => ui.showError(err));
-    }
-
-    ui.clearInput();// input temizlmee
-    e.preventDefault();
+  ui.clearInput(); // input temizlmee
+  e.preventDefault();
 }
-function clearAllSearched(){
- // son arananları temilze
- if(confirm("Emin misiniz ?")){
-    Storage.clearAllSearchedUsersFromStorage();// storageden temizleyecek
+function clearAllSearched() {
+  // son arananları temilze
+  if (confirm("Emin misiniz ?")) {
+    Storage.clearAllSearchedUsersFromStorage(); // storageden temizleyecek
     ui.clearAllSearchedFromUI();
- }
+  }
 }
-function getAllSearched(){
-    // arananları storagedan al ve uı a ekle,
-    
-    let users =Storage.getSearchedUsersFromStorage();
-    let result="";
-    users.forEach(user=>{
-        result +=`
+function getAllSearched() {
+  // arananları storagedan al ve uı a ekle,
+
+  let users = Storage.getSearchedUsersFromStorage();
+  let result = "";
+  users.forEach((user) => {
+    result += `
         <li class="list-group-item">${user}</li> 
         `;
-    });
-    lastUsers.innerHTML= result;
+  });
+  lastUsers.innerHTML = result;
 }
--- github-proje/storage.js
+++ github-proje/storage.js
@@ -1,31 +1,26 @@
-class Storage{
-    constructor(){
-        
+class Storage {
+  constructor() {}
+  static getSearchedUsersFromStorage() {
+    //tüm kullanıclları al
+    let users;
+    if (localStorage.getItem("searched") === null) {
+      users = [];
+    } else {
+      users = JSON.parse(localStorage.getItem("searched"));
     }
-    static getSearchedUsersFromStorage(){
-        //tüm kullanıclları al
-        let users;
-        if(localStorage.getItem("searched")===null)
-        {
-            users= [];
-        }
-        else{
-            users = JSON.parse(localStorage.getItem("searched"));
-        }
-        return users;
-    }
-    static addSearchedUserToStorage(username){
-        // kullanıcı ekle ındexoff
+    return users;
+  }
+  static addSearchedUserToStorage(username) {
+    // kullanıcı ekle ındexoff
 
-        let users = this.getSearchedUsersFromStorage();
-        if(users.indexOf(username)===-1){
-            users.push(username);
-        }
-        localStorage.setItem("searched",JSON.stringify(users))
-        
+    let users = this.getSearchedUsersFromStorage();
+    if (users.indexOf(username) === -1) {
+      users.push(username);
     }
-    static clearAllSearchedUsersFromStorage(){
-        // tüm kullanıcıları sil
-        localStorage.removeItem("searched");
-    }
+    localStorage.setItem("searched", JSON.stringify(users));
+  }
+  static clearAllSearchedUsersFromStorage() {
+    // tüm kullanıcıları sil
+    localStorage.removeItem("searched");
+  }
 }
